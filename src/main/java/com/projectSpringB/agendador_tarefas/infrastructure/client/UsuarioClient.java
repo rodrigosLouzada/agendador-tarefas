fb589e99eb3ee4e7c9f7ebb084dcca6594a2c0c8
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-05-30 12:30:22 -0300

    criando a interface usando feing e seus metodos, incompleto

---
+++ src/main/java/com/projectSpringB/agendador_tarefas/infrastructure/client/UsuarioClient.java
@@ -0,0 +1,14 @@
+package com.projectSpringB.agendador_tarefas.infrastructure.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "usuario" , url = "${usuario.url}")
+public interface UsuarioClient {
+
+    @GetMapping
+    UsuarioDTO buscarUsuarioPorEmail(@RequestParam("email") String email,
+                                     @RequestHeader("Authorization") String Token);
+}
