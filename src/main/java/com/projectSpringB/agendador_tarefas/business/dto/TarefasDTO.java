dc9ee7247c800d8531c0bc3af09439781db7a2e6
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-06-04 11:26:55 -0300

    correcoes e funcionando entre aplicacoes

--- src/main/java/com/projectSpringB/agendador_tarefas/business/dto/TarefasDTO.java
+++ src/main/java/com/projectSpringB/agendador_tarefas/business/dto/TarefasDTO.java
@@ -1,5 +1,6 @@
 package com.projectSpringB.agendador_tarefas.business.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.projectSpringB.agendador_tarefas.infrastructure.enums.StatusNotificacaoEnum;
 import lombok.*;
 
@@ -16,6 +17,7 @@ public class TarefasDTO {
     private String nomeTarefa;
     private String descricao;
     private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime dataEvento;
     private String emailUsuario;
     private LocalDateTime dataAlteracao;
